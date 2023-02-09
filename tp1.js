04535b16c8797baccbe857e81424926d4f98516e
lucasvallone <lucasvallone@gmail.com>
2023-02-08 22:45:17 -0300

    segunda parte del proyecto

--- tp1.js
+++ tp1.js
@@ -8,7 +8,27 @@ function productoSeleccionado(numeroId) {
         return alert("El producto seleccionado no existe.")
     }
 }
+let nombre_usuario = prompt("Ingresá tu nombre de usuario:");
+let edad = prompt("Ingresa tu edad: ");
+let direccion = prompt("Ingresa tu direccion")
 
+class usuarioNuevo {
+    constructor(usuario, edad) {
+        this.nombre_usuario = usuario;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+    get_datos() {
+        console.log("-->Datos del usuario<--")
+        console.log("Nombre:", this.nombre_usuario);
+        console.log("Edad:", this.edad);
+        console.log("Direccion:", this.direccion);
+    }
+
+
+}
+let usuario = new usuarioNuevo(nombre_usuario, edad, direccion)
+//Pide Datos de usuario y devuelve por consola.
 
 let productos = [
     { id: 1, nombre: "Gabinete gamer", precio: 30000 },
@@ -27,18 +47,15 @@ let totalProductos = 0;
 let producto = "";
 let otroProducto = "";
 let continuarCompra = "";
-let precioFinal = 0 ;
+let precioFinal = 0;
 
-//Pide nombre de usuario y devuelve por consola.
-let nombre_usuario = prompt("Ingresá tu nombre de usuario:");
-console.log("Bienvenido/a: ", nombre_usuario);
 
 //Inicia la compra.
 while (continuarCompra != "n") {
     producto = prompt("Seleccione un producto de la lista: \n 1.Gabinete gamer ($30000) \n 2.Memoria ram 8gb. ($12000) \n 3.Mouse. ($6000) \n 4.Procesador ryzen 7 ($108000) \n 5.Teclado ($10000)");
-  //Convierte lo ingresado por prompt en number.
+    //Convierte lo ingresado por prompt en number.
     let numeroId = parseInt(producto);
-//Filtrado de productos.
+    //Filtrado de productos.
     function buscarProducto(producto) {
         return producto.id == numeroId
     }
@@ -46,8 +63,8 @@ while (continuarCompra != "n") {
     let resultado_find = productos.find(buscarProducto);
     if (productoSeleccionado(numeroId)) {
         console.log("El producto seleccionado es: ", resultado_find);
-                //Acumulador de precio final.
-                precioFinal= precioFinal += resultado_find.precio;
+        //Acumulador de precio final.
+        precioFinal = precioFinal += resultado_find.precio;
         //Contador de productos.  (maximo 3)
         totalProductos = totalProductos += 1;
         if (totalProductos === 3) {
@@ -60,33 +77,34 @@ while (continuarCompra != "n") {
         continuarCompra = prompt("Desea otro producto? s/n")
         if (productoSeleccionado(otroProducto)) {
             otroProducto = prompt("Seleccione un producto de la lista: \n 1.Gabinete gamer ($30000) \n 2.Memoria ram 8gb. ($12000) \n 3.Mouse. ($6000) \n 4.Procesador ryzen 7 ($108000) \n 5.Teclado ($10000)");
-           //Convierte lo ingresado por prompt en number.
+            //Convierte lo ingresado por prompt en number.
             let numeroId = parseInt(otroProducto);
             //Filtrado de productos.
             function buscarProducto(otroProducto) {
                 return otroProducto.id == numeroId
             }
-            
+
             let resultado_find = productos.find(buscarProducto);
             if (productoSeleccionado(numeroId)) {
                 console.log("El producto seleccionado es: ", resultado_find);
                 //Acumulador de precio final.
-                precioFinal= precioFinal += resultado_find.precio;
+                precioFinal = precioFinal += resultado_find.precio;
                 //Contador de productos. (maximo 3)
                 totalProductos = totalProductos += 1;
                 if (totalProductos === 3) {
                     console.log("El carrito esta lleno!")
                     break;
                 }
-                
+
             }
         }
     }
-    
+
     else {
-        
+
     }
 }
-console.log ("Precio final: $",precioFinal)
+usuario.get_datos()
+console.log("Precio final: $", precioFinal)
 console.log("Total de productos en el carrito: ", totalProductos)
-console.log( nombre_usuario, ", Gracias por su compra.")
+console.log(nombre_usuario, ", Gracias por su compra.")
